308dae0483b3e14899577bacfb343dbee267be60
Angela Tsang <tsangangela_@hotmail.com>
2018-08-08 19:49:58 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Enter
+function enter() {
+//    document.getElementById("outside").style.opacity = 0;
+    document.getElementById("inside").style.opacity = 1;
+    document.getElementById("outside").style.visibility = "hidden";
+    document.getElementById("inside").style.visibility = "visible";
+}
+
+// Exit
+function exit() {
+    window.close();
+}
+
