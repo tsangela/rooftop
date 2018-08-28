a7803aa0819429a4ce19dc2610c9f7f967c0c561
Angela Tsang <tsangangela_@hotmail.com>
2018-08-28 15:53:53 -0700

    Clean up

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Enter
 function enter() {
-//    document.getElementById("outside").style.opacity = 0;
     document.getElementById("inside").style.opacity = 1;
     document.getElementById("outside").style.visibility = "hidden";
     document.getElementById("inside").style.visibility = "visible";
